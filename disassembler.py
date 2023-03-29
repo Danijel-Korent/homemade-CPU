7e2d1c5a8f82a8cd3f95a77c50d3e06511189f57
Danijel Korent <danijel.korent@gmail.com>
2023-03-29 18:14:16 +0200

    Added printing of instruction table, func for searching the table and func for parsing the .hex file

--- disassembler.py
+++ disassembler.py
@@ -34,3 +34,56 @@ instruction_table = [
     InstructionRow(0x0C, "JNE", OperandType.ROM_ADDRESS, OperandType.NONE)
 ]
 
+
+def find_instruction_by_opcode(opcode):
+    for instruction in instruction_table:
+        if instruction.instruction_opcode == opcode:
+            return instruction
+    return None
+
+
+# TODO: Implement
+def opcode_to_asm(opcode):
+    print(f"PARSING: {hex(opcode)}")
+    pass
+
+
+def parse_file(file_name):
+    file_name = "useless_OS.hex"
+
+    with open(file_name, "r") as file:
+        first_line = file.readline().strip()
+
+        if first_line != "v2.0 raw":
+            print("Wrong format!!")
+            return
+
+        for line in file:
+            hex_numbers = line.strip().split(" ")
+
+            for hex_number in hex_numbers:
+                try:
+                    decimal_number = int(hex_number, 16)
+                    opcode_to_asm(decimal_number)
+                except ValueError:
+                    print(f"Invalid hexadecimal number: {hex_number}")
+                    return
+
+
+if __name__ == '__main__':
+
+    # Print the table in a formatted way
+    print("instruction_opcode | instruction_name | operand1_type | operand2_type")
+    print("-" * 55)
+    for entry in instruction_table:
+        print(f"{entry.instruction_opcode:>17x} | {entry.instruction_name:<15} | {entry.operand1_type.value:<13} | {entry.operand2_type.value}")
+
+    # Test find_instruction_by_opcode
+    opcode_to_find = 0x02
+    found_instruction = find_instruction_by_opcode(opcode_to_find)
+    if found_instruction:
+        print(f"\nFound instruction: {found_instruction}")
+    else:
+        print(f"\nNo instruction found for opcode: {opcode_to_find:x}")
+
+    parse_file(None)
